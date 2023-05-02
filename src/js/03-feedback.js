05ee257cd5f8c7df3840e668367979d0a70d3931
j-Hop <119629763+j-Hop@users.noreply.github.com>
2023-05-03 01:08:38 +0300

    UPSIDE

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,32 +1,32 @@
 import throttle from 'lodash.throttle';
 // *
 const inputFormUrl = document.querySelector('.feedback-form');
-const emailInputUrl = document.querySelector('input');
-const messageInputUrl = document.querySelector('textarea');
 // ? // Додав селектори
 
 const FORM_STORAGE_KEY = 'feedback-form-state';
-const settingsArray = {
-  email: 'example@gmail.com',
-  message: 'message example...',
-};
+let formData = {};
 // ? // Створив ключ сховища та масив з параметрами
 
-if (localStorage.getItem(FORM_STORAGE_KEY)) {
-  const { email, message } = JSON.parse(localStorage.getItem(FORM_STORAGE_KEY));
-  emailInputUrl.value = email;
-  messageInputUrl.value = message;
+function addData (){
+  try{
+    const data =localStorage.getItem(FORM_STORAGE_KEY);
+    if (!data) return;
+    formData = JSON.parse(data);
+    Object.entries(formData).forEach(([key, value]) => {
+      inputFormUrl[key].value = value;
+    });
+  } catch(e){}
 }
+
+addData();
+
 // ? // Після перевірки на наявність даних в сховищі - додаються значення в інпути
 
 inputFormUrl.addEventListener('input', throttle(onFormInput, 500));
 
 function onFormInput(event) {
-  if (event.target.name === 'email' || event.target.name === 'message') {
-    settingsArray.email = emailInputUrl.value;
-    settingsArray.message = messageInputUrl.value;
-    localStorage.setItem(FORM_STORAGE_KEY, JSON.stringify(settingsArray));
-  }
+  formData[event.target.name] = event.target.value.trim();
+    localStorage.setItem(FORM_STORAGE_KEY, JSON.stringify(formData));
 }
 // ? // При інпутах працює функція що додає в масив параметрів поточні значенняя інпутів
 // ? Після чого додає масив поточних значень методом JSON.stringify в локальне сховище 
@@ -35,7 +35,8 @@ inputFormUrl.addEventListener('submit', onFormSubmit);
 
 function onFormSubmit(event) {
   event.preventDefault();
-  console.log(localStorage.getItem(FORM_STORAGE_KEY));
-  localStorage.clear();
-  inputFormUrl.reset()
+  console.log(formData);
+  formData = {};
+  localStorage.removeItem(FORM_STORAGE_KEY);
+  event.target.reset()
 }
